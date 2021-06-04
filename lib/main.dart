e6bd0fd136c600ddb5bd0e88349e87137827533d
toshiikuo <toshiikuo@gmail.com>
2021-06-04 09:47:13 +0900

    現在地に円をかけた。完成！

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
 import 'package:location/location.dart';
 
-
 void main() {
   runApp(MyApp());
 }
@@ -24,29 +23,19 @@ class MapApp extends StatefulWidget {
 }
 
 class _MapAppState extends State<MapApp> {
-
   LocationData locationData;
   Location location = new Location();
-  // LatLng loc;
 
   @override
   void initState() {
     super.initState();
     preLoc();
     getLoc();
-    // Future(() async {
-    //   loc = await getLoc();
-    // });
-    }
-
-
+  }
 
   Future<void> preLoc() async {
-
-
     bool _serviceEnabled;
     PermissionStatus _permissionGranted;
-    LocationData locationData;
 
     _serviceEnabled = await location.serviceEnabled();
     if (!_serviceEnabled) {
@@ -64,58 +53,47 @@ class _MapAppState extends State<MapApp> {
       }
     }
   }
-  Future<LatLng> getLoc() async{
+
+  Future<LatLng> getLoc() async {
     locationData = await location.getLocation();
-    //todo:緯度経度を円の中心に設定するために非同期処理を使う。これを勉強　https://dart.dev/codelabs/async-await
     print("location is... ${locationData.latitude}");
     return LatLng(locationData.latitude, locationData.longitude);
-
-
-
   }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<LatLng>(
-      future: getLoc(),
-      builder: (context, AsyncSnapshot<LatLng> snapshot){
-        if (snapshot.hasData) {
-
-          return FlutterMap(
-            options: MapOptions(
-              center: snapshot.data,
-              zoom: 13.0,
-            ),
-            layers: [
-              TileLayerOptions(
-                  urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                  subdomains: ['a', 'b', 'c']
+        future: getLoc(),
+        builder: (context, AsyncSnapshot<LatLng> snapshot) {
+          if (snapshot.hasData) {
+            return FlutterMap(
+              options: MapOptions(
+                center: snapshot.data,
+                zoom: 13.0,
               ),
-              CircleLayerOptions(
-                circles: [
-                  CircleMarker(
-                    color: Colors.yellow.withOpacity(0.7),
-                    radius: 100,
-                    borderColor: Colors.white.withOpacity(0.9),
-                    borderStrokeWidth: 2,
-                    //todo:LocationDataをLatlngに変換して円の中心に設定
-                    point: snapshot.data,
-                    // point: LatLng(51.5, -0.09),
-                    useRadiusInMeter: true,
-                  ),
-
-                ],
-              ),
-            ],
-          );
-        }
-        else {
-          return CircularProgressIndicator();
-        }
-      }
-    );
-
-
-
+              layers: [
+                TileLayerOptions(
+                    urlTemplate:
+                        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    subdomains: ['a', 'b', 'c']),
+                CircleLayerOptions(
+                  circles: [
+                    CircleMarker(
+                      color: Colors.yellow.withOpacity(0.7),
+                      radius: 100,
+                      borderColor: Colors.white.withOpacity(0.9),
+                      borderStrokeWidth: 2,
+                      point: snapshot.data,
+                      // point: LatLng(51.5, -0.09),
+                      useRadiusInMeter: true,
+                    ),
+                  ],
+                ),
+              ],
+            );
+          } else {
+            return CircularProgressIndicator();
+          }
+        });
   }
 }
